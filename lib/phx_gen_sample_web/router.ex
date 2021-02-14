3dc1e9c987ddf69865cc206bce33e67105a7d928
Tony Collen <tony@halogenlabs.com>
2021-02-14 14:20:15 -0600

    Add user route

--- lib/phx_gen_sample_web/router.ex
+++ lib/phx_gen_sample_web/router.ex
@@ -17,6 +17,8 @@ defmodule PhxGenSampleWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+
+    resources "/users", UserController
   end
 
   # Other scopes may use custom stacks.
